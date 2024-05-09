662aeadcd3fec2bdf8c1fa1e1ae34194dac4aa01
franklin-raph <igboekwulusifranklin@gmail.com>
2024-05-09 13:11:21 +0100

    update

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -3,18 +3,18 @@ import {useState} from 'react'
 const Login = () => {
   return (
     <div>
-      <div className='flex flex-col items-center justify-center h-[100vh]'>
-        <p>Admin Login</p>
-        <div>
+      <div className='flex flex-col items-center justify-center h-[100vh] w-[30%] mx-auto'>
+        <p className='font-[600] mb-5 text-[20px]'>Thaka Admin Login</p>
+        <div className='w-full'>
           <div>
-            <label className='block'>Email</label>
-            <input type='text' placeholder='johndoe@gmail.com'/>
+            <label className='block mb-2'>Email</label>
+            <input type='text' placeholder='johndoe@gmail.com' className="border rounded-[6px] outline-none w-full placeholder:text-[#B6B6B6] px-4 py-2"/>
           </div>
-          <div>
-            <label className='block'>Password</label>
-            <input type='password' placeholder='****' />
+          <div className='w-full mt-5'>
+            <label className='block mb-2'>Password</label>
+            <input type='password' placeholder='****' className="border rounded-[6px] outline-none w-full placeholder:text-[#B6B6B6] px-4 py-2"/>
           </div>
-          <button>Login</button>
+          <button className='bg-green-500 w-full mt-3 py-2 rounded-[4px] text-white'>Login</button>
         </div>
       </div>
     </div>
